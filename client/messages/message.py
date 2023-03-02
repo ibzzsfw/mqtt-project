30eed934b638b6ed676e1ffb655437542c3269ff
jate-koh <jate_ben@hotmail.com>
2023-03-02 20:57:48 +0700

    `refactor`: make function params optional

--- client/messages/message.py
+++ client/messages/message.py
@@ -3,18 +3,43 @@ import json
 class Message:
     
     # Constructor to initialize message
-    def __init__(self, type, message, timestamp):
+    def __init__(self, type, content, timestamp):
         self.time = timestamp
-        self.message = message
+        self.content= content
         self.type = type
+        self.createMessage()
+        self.toJson()
     
-    def createMessage():
-        temp = {
-            "time": self.time,
-            "type": self.type,
-            "message": self.message
-        }
-        return temp
+    # Function to create message (dictionary format)
+    def createMessage(self, content, timestamp, type):
+        if (content == None & timestamp == None & type == None) :
+            temp = {
+                "time": timestamp,
+                "type": type,
+                "content": content
+            }
+        else :
+            temp = {
+                "time": self.time,
+                "type": self.type,
+                "content": self.content
+            }
+        self.message = temp
+        return self.message
+    
+    # Function to convert message to json format
+    def toJson(self, message):
+        if (message == None) :
+            self.json = json.dumps(self.createMessage())
+        else :
+            self.json = json.dumps(message)
+        return self.json
+    
+    """
+    Getter functions and Setter functions
+    """
+    def getMessage(self):
+        return self.message
 
-    def toJson(self):
-        return json.dumps(self.message)
+    def getJson(self):
+        return self.toJson()
