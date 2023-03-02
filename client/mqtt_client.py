36a0582a7ed5650a287ec4b4c660aabe8b457e7a
jate-koh <jate_ben@hotmail.com>
2023-03-02 20:59:10 +0700

    `feat`: add message init in publish func

--- client/mqtt_client.py
+++ client/mqtt_client.py
@@ -1,3 +1,28 @@
+"""
+ MQTT Client has three types of sensors 
+ 
+ – Relative humidity, temperature, and thermal array. 
+    - The relative humidity readings are between 0 to 100 percent, 
+    - the temperature readings are between 0 to 90 degree celsius, 
+    - the thermal array readings are 24×32 where each array value represents a temperature reading between 5 to 60 degree celsius. 
+ 
+ - Client simultaneously reads all the sensors every 3 minutes 
+ - and wants to send the sensor data to Broker together with 4-digit node id and current time (Date, hours, minutes). 
+ 
+ To emulate the sensor readings without installing real sensors, Client can read sensor data stored in an Excel file and sends to Broker.
+ Constraints:
+- Client can only send at most 250 bytes in one message.
+"""
+
+# Import libraries
+import paho.mqtt.client as mqtt
+
+# Import modules
+from client.messages.message import Message
+from client.messages.thermal import Thermal
+from client.messages.humidity import Humidity
+from client.messages.thermal_array import ThermalArray
+from client.messages.temperature import Temperature
 
 class MqttClient:
 
@@ -28,8 +53,18 @@ class MqttClient:
     self: MqttClient
     message: string
     """
-    def publish(self, message):
-        self.client.publish(self.topic, message)
+    def publish(self, type, content, timestamp):
+        if type == 'thermal':
+            message = Thermal(content, timestamp)
+        elif type == 'humidity':
+            message = Humidity(content, timestamp)
+        elif type == 'temperature':
+            message = Temperature(content, timestamp)
+        elif type == 'thermal_array':
+            message = ThermalArray(content, timestamp)
+        else: 
+            message = Message('other', content, timestamp)
+        self.client.publish(self.topic, message.getJson())
 
     """
     Function to subscribe to topic
